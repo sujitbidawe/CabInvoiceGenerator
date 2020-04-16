ffb75bfc6d59af4b97ce6cb14c5150650c6a65ed
sujitbidawe <bidawesujit@gmail.com>
2020-04-16 16:24:28 +0530

    added: test case to check total fare when given multiple rides

--- src/test/java/InvoiceServiceTest.java
+++ src/test/java/InvoiceServiceTest.java
@@ -11,5 +11,23 @@ public class InvoiceServiceTest {
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 0.0);
     }
+
+    @Test
+    public void givenLessDistanceOrTime_shouldReturnMiniumFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_shouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = { new Ride(2.0, 5),
+                         new Ride(0.1, 1)};
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(30, fare, 0.0);
+    }
 }
 
