24f2454296ea20fc61dc79064b52fc80cefbfb7c
sujitbidawe <bidawesujit@gmail.com>
2020-04-16 16:25:10 +0530

    added: code for Ride.java

---
+++ src/main/java/Ride.java
@@ -0,0 +1,9 @@
+public class Ride {
+    public double distance;
+    public final int time;
+
+    public Ride(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
+}
